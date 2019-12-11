e6786ff4746b5aa1b31f6abae53e6cf28243ad32
Yeonjong Bong <yj24.bong@gmail.com>
2019-12-11 10:24:28 +0900

    Modified boj1462 code

--- boj/1462/1462.cpp
+++ boj/1462/1462.cpp
@@ -1,77 +1,69 @@
 #include <cstdio>
 #include <queue>
 #include <utility>
-#define INF 100000000000000000ll
-
+#define MAX_N 500000
+#define INF 1000000000000000ll
 using namespace std;
 typedef long long ll;
-typedef pair<ll,ll> ii;
-
-int n; // 전체 문제의 수
-int m; // 보너스 점수를 얻기 위해 필요한 코인의 수
-
-// 문제 번호는 1 ~ n 번
-ll p[500001]; // p[i] = i번 문제를 맞추거나 틀릴 때 가감되는 점수
-ll b[500001]; // b[i] = i번 문제를 맞춰서 코인이 m개가 되었을 때 얻는 보너스 점수
-
-ll s[500001]; // s[i] = p[1]+p[2]+ ... +p[i]
-ll d[500001]; // d[i] = i번 문제를 풀어서 코인이 0개가 된 경우에 그때까지 얻을 수 있는 최대 점수
-
-priority_queue <ii> pq; // max heap
-
+typedef pair<ll,int> ii;
 ll max2(ll a, ll b){ return a>b?a:b; }
-int main(void){
 
-	// 입력
-	scanf("%d %d",&n,&m);
-	for(int i=1; i<=n; i++) scanf("%lld",&p[i]);
-	for(int i=1; i<=n; i++) scanf("%lld",&b[i]);
+int N; // 문제의 개수(1~N번 문제)
+int M; // 보너스 점수를 얻기 위해 필요한 코인의 수
+ll point[MAX_N+1]; // point[i] = i번 문제를 맞히거나 틀렸을 때 가감되는 점수
+ll sumPoint[MAX_N+1]; // sumPoint[i] = point[1]+...+point[i]
+ll bonus[MAX_N+1]; // bonus[i] = i번 문제의 보너스 점수
 
-	// s배열 계산
-	s[0]=0;
-	for(int i=1; i<=n; i++) s[i]=s[i-1]+p[i];
+ll D[MAX_N+1]; // D[i] = 1~i번 문제까지 풀고 나서 코인이 0개인 상황일 때의 가능한 최대 점수
+priority_queue <ii> pq; // element의 first: D[k]-sumPoint[k], second: k
 
-	// d배열 초기화
-	d[0]=0;
-	for(int i=1; i<=n; i++) d[i]=-INF;
+int main() {
 
-	// max heap 초기화
-	pq.push(make_pair(d[0]-s[0], 0));
+    // 입력
+    scanf("%d %d",&N,&M);
+    for(int i=1; i<=N; i++) scanf("%lld",&point[i]);
+    for(int i=1; i<=N; i++) scanf("%lld",&bonus[i]);
 
-	// d배열 계산
-	for(int i=1; i<=n; i++){
+    // sumPoint
+    sumPoint[0]=0;
+    for(int i=1; i<=N; i++) sumPoint[i]=sumPoint[i-1]+point[i];
 
-		if(i-m>=0) d[i]=max2(d[i],d[i-m]+s[i]-s[i-m] + b[i]);
+    // D 배열 계산
+    for(int i=0; i<=N; i++) D[i]=-INF;
+    D[0]=0;
+    pq.push(make_pair(D[0]-sumPoint[0], 0));
 
-		// 이 부분은 max heap으로 변경 필요
-		/*
-		for(int j=max2(i-m+1,0); j<i; j++){
-			d[i]=max2(d[i],d[j]+s[i-1]-s[j] - p[i]);
-		}
-		*/
+    for(int i=1; i<=N; i++){
 
-		// max heap 이용
-		ll tmp=-INF;
-		while(!pq.empty()){
-			ii t=pq.top();
-			if(t.second >= max2(i-m+1, 0)){
-				tmp=t.first;
-				break;
-			}
-			else pq.pop();
-		}
-		d[i]=max2(d[i], tmp+s[i-1]-p[i]);
+        // case 1: i번 문제를 틀려서 코인 0개가 됨
+        ll val1=-INF;
+        while(!pq.empty()){
+            ii f=pq.top();
+            if(f.second>=max2(0,i-M)){
+                val1=f.first;
+                break;
+            }
+            else pq.pop();
+        }
+        val1+=sumPoint[i-1]-point[i];
 
-		// max heap 업데이트
-		pq.push(make_pair(d[i]-s[i], i));
-	}
-	
-	// 답 계산
-	ll ans=-INF;
-	for(int i=max2(n-m+1,0); i<=n; i++){
-		ans=max2(ans,d[i]+s[n]-s[i]);		
-	}
-	printf("%lld\n",ans);
+        // case 2: i번 문제를 맞혀서 코인 0개가 됨 (즉, i-M번 문제에서 코인이 0개였고 i-M+1~i번 문제를 모두 맞혀서 i번 문제의 보너스 점수를 받음)
+        ll val2=-INF;
+        if(i>=M) val2=D[i-M]+sumPoint[i]-sumPoint[i-M]+bonus[i];
 
-	return 0;
+        // case 1과 2 중 더 큰 값을 취함
+        D[i]=max2(D[i], val1);
+        D[i]=max2(D[i], val2);
+
+        // pq (max heap) 업데이트
+        pq.push(make_pair(D[i]-sumPoint[i], i));
+    }
+
+    // 답 계산
+    ll ans=D[N]; // 정의에 따라 D[N]은 N번 문제에서 코인 0개일 때의 최대 점수이므로, N번 문제를 맞혔으나 N번 문제의 보너스 점수를 받지 못한 경우는 고려하지 않은 것이다. 아래에서 그런 경우를 고려한다.
+    for(int k=max2(0,N-M+1); k<N; k++)
+        ans=max2(ans, D[k]+sumPoint[N]-sumPoint[k]);
+    printf("%lld\n",ans);
+
+    return 0;
 }
